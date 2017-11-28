5e41b3445c7e08e015c916a416c596e3a15cd8d4
Felipe Gorostiaga <feligorostiaga@gmail.com>
2017-11-28 11:38:16 +0100

    creating up to tjob

--- e2e-test/start-instance.sh
+++ e2e-test/start-instance.sh
@@ -2,6 +2,7 @@
 
 ELASTESTURL=localhost:37000
 
+# Posting EMS instance
 INSTANCEID=$(curl -s -XPOST http://$ELASTESTURL/api/esm/services/bab3ae67-8c1d-46ec-a940-94183a443825/prov)
 
 EMSURL=null
@@ -15,5 +16,28 @@ done
 
 EMSURL=$(echo "$EMSURL" | tr -d '"')
 echo EMS URL: $EMSURL
+EMSIPPORT=$(echo "$EMSURL" | sed 's/http:..//;s/:.*//')":5044"
+echo EMS IP AND PORT: $EMSIPPORT
 
-curl -H "Content-Type: application/json" -d '{ "channel": "chan", "ip": "localhost", "port": 9201, "user": "elastic", "password": "changeme" }' "${EMSURL}subscriber/elasticsearch"
+# project creation
+echo Creating project
+PROJ=$(curl -s -H "Content-Type: application/json" -d '{ "id": 0, "name": "proyecto" }' "http://$ELASTESTURL/api/project")
+echo $PROJ
+
+# SuT creation
+echo Creating SuT
+SUT=$(curl -s -H "Content-Type: application/json" -d '{ "id": 1, "name": "dename", "specification": "williamyeh/dummy", "sutType": "MANAGED", "description": "dedescript", "project": '${PROJ}', "instrumentalize": false, "currentSutExec": 1, "instrumentedBy": "WITHOUT", "port": null, "managedDockerType": "IMAGE", "mainService": "mbeat", "parameters": [] }' "http://$ELASTESTURL/api/sut")
+echo $SUT
+
+# T-Job creation
+echo Creating T-Job
+DATA='{ "id": 0, "name": "nombredeltjob", "imageName": "docker.elastic.co/beats/metricbeat:5.4.0", "sut": '"${SUT}"', "project": '"${PROJ}"' , "tjobExecs": [], "parameters": [], "commands": "-e -E output.logstash.hosts=[\"'"${EMSIPPORT}"'\"]", '"$(cat suffix.json)"
+TJOB=$(curl -s -H "Content-Type: application/json" -d "${DATA}" "http://$ELASTESTURL/api/tjob")
+echo $TJOB
+
+
+# tjob commands: metricbeat -e -E output.logstash.hosts=['172.19.0.14:5044'] -E output.elasticsearch.hosts=['edm-elasticsearch:9200']
+# tjob environment docker image: docker.elastic.co/beats/metricbeat:5.4.0
+
+# subscriber. Doesn't seem necessary anymore
+# curl -H "Content-Type: application/json" -d '{ "channel": "chan", "ip": "edm-elasticsearch", "port": 9201, "user": "elastic", "password": "changeme" }' "${EMSURL}subscriber/elasticsearch" # might be wrong..
