763f9353e77a2492c85c82271108f7c29a65cc5d
fgorostiaga <feligorostiaga@gmail.com>
2017-11-29 10:17:26 +0100

    Reading elastest url from arg (#54)

--- e2e-test/start-instance.sh
+++ e2e-test/start-instance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ELASTESTURL=localhost:37000
+ELASTESTURL=$1
 
 # Posting EMS instance
-INSTANCEID=$(curl -s -XPOST http://$ELASTESTURL/api/esm/services/bab3ae67-8c1d-46ec-a940-94183a443825/prov)
+INSTANCEID=$(curl -s -XPOST $ELASTESTURL/api/esm/services/bab3ae67-8c1d-46ec-a940-94183a443825/prov)
 
 EMSURL=null
 
@@ -11,7 +11,7 @@ while [ "X$EMSURL" = "Xnull" ];
 do 
     echo Waiting and retrying..
     sleep 1
-    EMSURL=$(curl -s http://$ELASTESTURL/api/esm/services/instances/$INSTANCEID | docker run -i --rm nimmis/jq .urls.api)
+    EMSURL=$(curl -s $ELASTESTURL/api/esm/services/instances/$INSTANCEID | docker run -i --rm nimmis/jq .urls.api)
 done
 
 EMSURL=$(echo "$EMSURL" | tr -d '"')
@@ -21,24 +21,24 @@ echo EMS IP AND PORT: $EMSIPPORT
 
 # project creation
 echo Creating project
-PROJ=$(curl -s -H "Content-Type: application/json" -d '{ "id": 0, "name": "proyecto" }' "http://$ELASTESTURL/api/project")
+PROJ=$(curl -s -H "Content-Type: application/json" -d '{ "id": 0, "name": "proyecto" }' "$ELASTESTURL/api/project")
 echo $PROJ
 
 # SuT creation
 echo Creating SuT
-SUT=$(curl -s -H "Content-Type: application/json" -d '{ "id": 1, "name": "dename", "specification": "williamyeh/dummy", "sutType": "MANAGED", "description": "dedescript", "project": '${PROJ}', "instrumentalize": false, "currentSutExec": 1, "instrumentedBy": "WITHOUT", "port": null, "managedDockerType": "IMAGE", "mainService": "mbeat", "parameters": [] }' "http://$ELASTESTURL/api/sut")
+SUT=$(curl -s -H "Content-Type: application/json" -d '{ "id": 1, "name": "dename", "specification": "williamyeh/dummy", "sutType": "MANAGED", "description": "dedescript", "project": '${PROJ}', "instrumentalize": false, "currentSutExec": 1, "instrumentedBy": "WITHOUT", "port": null, "managedDockerType": "IMAGE", "mainService": "mbeat", "parameters": [] }' "$ELASTESTURL/api/sut")
 echo $SUT
 
 # T-Job creation
 echo Creating T-Job
 DATA='{ "id": 0, "name": "nombredeltjob", "imageName": "docker.elastic.co/beats/metricbeat:5.4.0", "sut": '"${SUT}"', "project": '"${PROJ}"' , "tjobExecs": [], "parameters": [], "commands": "metricbeat -e -E output.logstash.hosts=[\"'"${EMSIPPORT}"'\"] -E output.elasticsearch.hosts=[\"edm-elasticsearch:9200\"]", '"$(cat suffix.json)"
-TJOB=$(curl -s -H "Content-Type: application/json" -d "${DATA}" "http://$ELASTESTURL/api/tjob")
+TJOB=$(curl -s -H "Content-Type: application/json" -d "${DATA}" "$ELASTESTURL/api/tjob")
 TJOBID=$(echo $TJOB | docker run -i --rm nimmis/jq .id)
 echo TJOBID: $TJOBID
 
 # T-Job execution
 echo Executing T-Job
-TJOBEXEC=$(curl -s -H "Content-Type: application/json" -d '{"tJobParams": []}' "http://$ELASTESTURL/api/tjob/$TJOBID/exec")
+TJOBEXEC=$(curl -s -H "Content-Type: application/json" -d '{"tJobParams": []}' "$ELASTESTURL/api/tjob/$TJOBID/exec")
 echo $TJOBEXEC
 
 # Waiting for the T-Job to start producing events
