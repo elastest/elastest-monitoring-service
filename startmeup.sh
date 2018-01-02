a2d3408b7abb71c9ac2413b561068325edf1e0cf
fgorostiaga <feligorostiaga@gmail.com>
2018-01-02 13:28:11 -0300

    Added elasticsearch to host (#61)

--- startmeup.sh
+++ startmeup.sh
@@ -9,6 +9,10 @@ mkfifo /usr/share/logstash/pipes/swageventspipe
 mkdir /usr/share/logstash/in_data
 mkdir /usr/share/logstash/out_data
 
+EDM_ES=$(echo $ET_EDM_ELASTICSEARCH_API | sed 's/http:..//;s/:.*//')
+ES_IP=$(ping -c 1 $EDM_ES | head -n1 | sed 's/).*//;s/.*(//')
+echo -e "$ES_IP\telasticsearch" >> /etc/hosts
+
 logstash -f /usr/share/logstash/pipeline/inlogstash.conf --path.data /usr/share/logstash/in_data &
 
 logstash -f /usr/share/logstash/pipeline/outlogstash.conf --config.reload.automatic --path.data /usr/share/logstash/out_data &
