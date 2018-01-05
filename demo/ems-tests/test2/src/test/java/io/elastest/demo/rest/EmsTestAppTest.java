512a20f0f9c4ff1dabc023175fb82e8982790045
Cesar Sanchez <cesar.sanchez@imdea.org>
2018-01-05 18:28:10 +0100

    pom with springframework dependencies

--- demo/ems-tests/test2/src/test/java/io/elastest/demo/rest/EmsTestAppTest.java
+++ demo/ems-tests/test2/src/test/java/io/elastest/demo/rest/EmsTestAppTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http;
+import org.springframework.http.HttpHeaders;
 
 
 public class EmsTestAppTest {
