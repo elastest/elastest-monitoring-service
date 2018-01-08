c80568a60afb31442288e7dd07503fa026645575
fgorostiaga <feligorostiaga@gmail.com>
2018-01-08 12:38:13 -0300

    reverted sut1 (#85)

--- demo/suts/sut1/stress_periodically.sh
+++ demo/suts/sut1/stress_periodically.sh
@@ -1,8 +1,6 @@
 #/bin/bash
 while true; do
-    curl -H "content-type: application/json" -XPUT 'http://${ET_EMS_LSBEATS_HOST}:8181' -d '{ "message" : "Stress", "status" : "on" }'
     timeout 5s yes > /dev/null
-    curl -H "content-type: application/json" -XPUT 'http://${ET_EMS_LSBEATS_HOST}:8181' -d '{ "message" : "Stress", "status" : "off" }'
     #echo sleeping..
     sleep 5
     #echo wakeup
