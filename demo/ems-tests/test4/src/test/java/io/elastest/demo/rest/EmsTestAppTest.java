8ce212f00cecf41670cf33be783db0c1d27e7779
Cesar Sanchez <cesar.sanchez@imdea.org>
2018-01-08 17:15:42 +0100

    fix

--- demo/ems-tests/test4/src/test/java/io/elastest/demo/rest/EmsTestAppTest.java
+++ demo/ems-tests/test4/src/test/java/io/elastest/demo/rest/EmsTestAppTest.java
@@ -45,11 +45,12 @@ public class EmsTestAppTest {
         RestTemplate client = new RestTemplate();
 
         String result = "0";
+		int processed_events = 0;
 
         int counter = 60;
 
 		int expected_events = 10000;
-        while (expected_events > Integer.parseInt(result) && counter > 0) {
+        while (expected_events > processed_events && counter > 0) {
 			String ems_api_url = "http://" + appHost + ":8888/health";
 			System.out.println("Connecting to "+ems_api_url+"...");
             result = client.getForObject(ems_api_url, String.class);
@@ -62,6 +63,8 @@ public class EmsTestAppTest {
 
 			System.out.println("...which corresponds to " + result + "events");
 
+			processed_events = Integer.parseInt(result);
+
             counter--;
             try {
                 System.out.println("sleeping for 3s...");
@@ -73,7 +76,6 @@ public class EmsTestAppTest {
 
         }
 		/* assertThat(result).isNotEqualTo("0");  */
-		assertThat(result).isGreaterThanOrEqual(expected_events); */
+		assertThat(processed_events).isGreaterThanOrEqual(expected_events); 
     }
-
 }
