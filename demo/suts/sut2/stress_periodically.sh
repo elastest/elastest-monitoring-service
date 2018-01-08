4c934884695a3b49b4a94320fa71e9d6e96b0568
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-01-08 16:54:46 +0100

    Added true to avoid crashing sut

--- demo/suts/sut2/stress_periodically.sh
+++ demo/suts/sut2/stress_periodically.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 while true; do
-    curl -H "content-type: application/json" -XPUT "http://${ET_EMS_LSBEATS_HOST}:8181" -d '{ "message" : "Stress", "status" : "on" }'
+    curl -H "content-type: application/json" -XPUT "http://${ET_EMS_LSBEATS_HOST}:8181" -d '{ "message" : "Stress", "status" : "on" }' || true
     timeout 5s yes > /dev/null
-    curl -H "content-type: application/json" -XPUT "http://${ET_EMS_LSBEATS_HOST}:8181" -d '{ "message" : "Stress", "status" : "off" }'
+    curl -H "content-type: application/json" -XPUT "http://${ET_EMS_LSBEATS_HOST}:8181" -d '{ "message" : "Stress", "status" : "off" }' || true
     #echo sleeping..
     sleep 5
     #echo wakeup
