d3763d9d651ee83debf62ff286595a23434cbb60
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-01-09 01:12:36 +0100

    Modified both suts

--- demo/suts/sut2/stress_periodically.sh
+++ demo/suts/sut2/stress_periodically.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 while true; do
     curl -H "content-type: application/json" -XPUT "http://${ET_EMS_LSBEATS_HOST}:8181" -d '{ "message" : "Stress", "status" : "on" }' || true
-    timeout 5s yes > /dev/null
+    timeout 30s yes > /dev/null
     curl -H "content-type: application/json" -XPUT "http://${ET_EMS_LSBEATS_HOST}:8181" -d '{ "message" : "Stress", "status" : "off" }' || true
     #echo sleeping..
-    sleep 5
+    sleep 30
     #echo wakeup
 done
