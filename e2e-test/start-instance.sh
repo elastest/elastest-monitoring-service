faca16ac8a26da9998a5685aa4e35c312da9eaba
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-02-28 10:50:58 +0100

    Removing sudo from start-instance

--- e2e-test/start-instance.sh
+++ e2e-test/start-instance.sh
@@ -11,7 +11,7 @@ while [ "X$EMSURL" = "Xnull" ];
 do 
     echo Waiting and retrying..
     sleep 1
-    EMSURL=$(curl -s $ELASTESTURL/api/esm/services/instances/$INSTANCEID | sudo docker run -i --rm nimmis/jq .urls.api)
+    EMSURL=$(curl -s $ELASTESTURL/api/esm/services/instances/$INSTANCEID | docker run -i --rm nimmis/jq .urls.api)
 done
 
 EMSURL=$(echo "$EMSURL" | tr -d '"')
@@ -33,7 +33,7 @@ echo $SUT
 echo Creating T-Job
 DATA='{ "id": 0, "name": "nombredeltjob", "imageName": "docker.elastic.co/beats/metricbeat:5.4.0", "sut": '"${SUT}"', "project": '"${PROJ}"' , "tjobExecs": [], "parameters": [], "commands": "metricbeat -e -E output.logstash.hosts=[\"'"${EMSIPPORT}"'\"] -E output.elasticsearch.hosts=[\"edm-elasticsearch:9200\"]", '"$(cat suffix.json)"
 TJOB=$(curl -s -H "Content-Type: application/json" -d "${DATA}" "$ELASTESTURL/api/tjob")
-TJOBID=$(echo $TJOB | sudo docker run -i --rm nimmis/jq .id)
+TJOBID=$(echo $TJOB | docker run -i --rm nimmis/jq .id)
 echo TJOBID: $TJOBID
 
 # T-Job execution
@@ -55,7 +55,7 @@ do
         exit 1
     fi
     sleep 5
-    PROCESSEDEVENTS=$(curl -s "${EMSURL}health" | sudo docker run -i --rm nimmis/jq fromjson.ProcessedEvents)
+    PROCESSEDEVENTS=$(curl -s "${EMSURL}health" | docker run -i --rm nimmis/jq fromjson.ProcessedEvents)
 done
 echo received $PROCESSEDEVENTS events
 exit 0
