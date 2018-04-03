fd1a104841dff3446f2426f21d591e0b07eefa08
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-04-03 15:20:38 +0200

    Demo modified with sleeps

--- demo/suts/sut4/startmeup.sh
+++ demo/suts/sut4/startmeup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 /usr/bin/printenv
+sleep 100
+curl -d "{ \"channel\": \"any\", \"ip\": \"elastest.software.imdea.org\", \"port\": 9202, \"user\": \"elastic\", \"password\": \"changeme\" }" -H "Content-Type: application/json" "http://${ET_EMS_LSBEATS_HOST}:8888/subscriber/elasticsearch"
+sleep 15
 /stress.sh &
 /usr/local/bin/docker-entrypoint -e
