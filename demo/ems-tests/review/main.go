67e779c285f6d249a91c8cdec328d2dd35cc9291
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-06-12 09:44:11 +0200

    Starting demo tjob

---
+++ demo/ems-tests/review/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type event struct {
+	Type          string `json:"type,omitempty"`
+	ContainerName string `json:"containerName,omitempty"`
+	Net           *Net   `json:"net,omitempty"`
+	Message       string `json:"message,omitempty"`
+	EmsType       string `json:"ems_type,omitempty"`
+}
+
+//Net net structure
+type Net struct {
+	TxBytesPs float64 `json:"txBytes_ps,omitempty"`
+}
+
+func main() {
+	ems := os.Getenv("ET_EMS_LSBEATS_HOST")
+	ems = fmt.Sprintf("%s:3232", ems)
+	u := url.URL{Scheme: "ws", Host: ems, Path: "/"}
+	log.Printf("Connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+		time.Sleep(300 * time.Second)
+	}
+	defer c.Close()
+	log.Printf("Done!")
+
+	done := make(chan struct{})
+	defer close(done)
+	log.Printf("Looping for metrics...")
+	for {
+		_, input, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+
+		var e event
+		json.Unmarshal(input, &e)
+		if e.Type == "net" {
+			if strings.HasSuffix(e.ContainerName, "full-teaching-openvidu-server-kms_1") {
+				//log.Printf("Checking %f\n", e.Net.TxBytesPs)
+				if e.Net.TxBytesPs > 100000.0 {
+					os.Exit(1)
+				}
+			}
+		}
+		if e.EmsType == "webrtc" {
+			log.Printf("8182 INPUT:\n        %s\n", input)
+		}
+		if strings.Contains(e.Message, "Finished at") {
+			os.Exit(0)
+		}
+	}
+}
---
+++ demo/ems-tests/review/tjob.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+git clone https://github.com/pchico83/full-teaching-experiment
+cd full-teaching-experiment
+mvn -Dtest=FullTeachingTestE2EVideoSession -B test &
+
+cd
+git clone https://github.com/fgorostiaga/elastest-monitoring-service
+cd elastest-monitoring-service
+git checkout review_demo
+
+cd demo/ems-tests/review
+echo Posting stamper
+curl -H "Content-Type:text/plain"  --data-binary @stampers.txt http://localhost:8888/stamper/tag0.1
+echo Posting sessions
+curl -H "Content-Type:text/plain" --data-binary @sessiondef.txt http://${ET_EMS_LSBEATS_HOST}:8888/MonitoringMachine/signals0.1
+echo "Building the GO binaries..."
+go build -o /usr/local/bin/tjob
+echo "Done!"
+
+echo "Executing Go agent..."
+/usr/local/bin/tjob
+exit $?
