685850a7f2f8ec7484a09ffbd573f0a683acf454
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-06-13 22:20:30 +0200

    only logging ws

--- demo/ems-tests/review/main.go
+++ demo/ems-tests/review/main.go
@@ -51,7 +51,9 @@ func main() {
 
 		var e event
 		json.Unmarshal(input, &e)
-        log.Printf("Received event:[%s]", string(input))
+		if strings.Contains(string(input), "#websocket") {
+            log.Printf("Received event:[%s]", string(input))
+		}
 		if e.Type == "net" {
 			if strings.HasSuffix(e.ContainerName, "full-teaching-openvidu-server-kms_1") {
 				//log.Printf("Checking %f\n", e.Net.TxBytesPs)
