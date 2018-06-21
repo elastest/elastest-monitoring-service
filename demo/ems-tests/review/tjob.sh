7a40df609b67346589ce35ddd8726dd53fb42b51
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-06-21 17:51:06 +0200

    Fixed tjob def

--- demo/ems-tests/review/tjob.sh
+++ demo/ems-tests/review/tjob.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/pchico83/full-teaching-experiment
 cd full-teaching-experiment
-mvn -Dtest=FullTeachingTestE2EVideoSession -B test &
+mvn -Dtest=FullTeachingTestE2EDualVideoSession -B test &
 
 cd
 git clone https://github.com/fgorostiaga/elastest-monitoring-service
@@ -11,7 +11,7 @@ git checkout review_demo
 
 cd demo/ems-tests/review
 echo Posting stamper
-curl -H "Content-Type:text/plain"  --data-binary @stampers.txt http://localhost:8888/stamper/tag0.1
+curl -H "Content-Type:text/plain"  --data-binary @stampers.txt http://${ET_EMS_LSBEATS_HOST}:8888/stamper/tag0.1
 echo Posting sessions
 curl -H "Content-Type:text/plain" --data-binary @sessiondef.txt http://${ET_EMS_LSBEATS_HOST}:8888/MonitoringMachine/signals0.1
 echo "Building the GO binaries..."
