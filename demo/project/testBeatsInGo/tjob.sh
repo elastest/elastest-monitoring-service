f69a7a4cf01b19ce1e2c162d647aee2341a907a1
Pablo Chico de Guzmán <pchico83@gmail.com>
2018-06-26 16:50:26 +0200

    Testing FullTeaching app (#145)

---
+++ demo/project/testBeatsInGo/tjob.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git clone https://github.com/elastest/full-teaching-experiment
+
+git clone https://github.com/elastest/elastest-monitoring-service
+mv elastest-monitoring-service/demo/project/FullTeachingTestE2EDualVideoSession.java full-teaching-experiment/src/test/java/com/fullteaching/backend/e2e/
+
+cd full-teaching-experiment
+mvn -Dtest=FullTeachingTestE2EDualVideoSession -B test &
+
+cd
+cd elastest-monitoring-service/demo/project/testBeatsInGo
+echo "Building the GO binaries..."
+go build -o /usr/local/bin/tjob
+echo "Done!"
+
+echo "Executing Go agent..."
+/usr/local/bin/tjob
+exit $?
