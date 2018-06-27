035c4f979b4becb02d72cfeb141e03de1cea9ede
Felipe Gorostiaga <feligorostiaga@gmail.com>
2018-06-27 11:09:10 +0200

    Redone tjob

--- demo/ems-tests/review/tjob.sh
+++ demo/ems-tests/review/tjob.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-git clone https://github.com/pchico83/full-teaching-experiment
+git clone https://github.com/elastest/full-teaching-experiment
+
+git clone https://github.com/elastest/elastest-monitoring-service
+mv elastest-monitoring-service/demo/project/FullTeachingTestE2EDualVideoSession.java full-teaching-experiment/src/test/java/com/fullteaching/backend/e2e/
+
 cd full-teaching-experiment
 mvn -Dtest=FullTeachingTestE2EDualVideoSession -B test &
 
 cd
-git clone https://github.com/fgorostiaga/elastest-monitoring-service
-cd elastest-monitoring-service
-git checkout review_demo
-
-cd demo/ems-tests/review
+cd elastest-monitoring-service/demo/ems-tests/review
 echo Posting stamper
 curl -H "Content-Type:text/plain"  --data-binary @stampers.txt http://${ET_EMS_LSBEATS_HOST}:8888/stamper/tag0.1
 echo Posting sessions
