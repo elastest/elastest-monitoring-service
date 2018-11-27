37550c0d19c9840ca030f9bfe543d4169dcc8e9b
fgorostiaga <feligorostiaga@gmail.com>
2018-11-27 16:42:36 -0300

    Migrating e2e tests (#151)
    * Updated e2e tests (cleaning up)
    
    * Updated Jenkinsfile

--- e2e-test/start-instance.sh
+++ e2e-test/start-instance.sh
@@ -2,20 +2,41 @@
 
 ELASTESTURL=$1
 
+function cleanexit() {
+    # Project destruction
+    echo Destroying project
+    curl -X "DELETE" "$ELASTESTURL/api/project/$PROJID"
+    exit -1
+}
+
+# Project creation
+echo Creating Project
+PROJ=$(curl -s -H "Content-Type: application/json" -d '{ "id": 0, "name": "EMSe2e" }' "$ELASTESTURL/api/project")
+echo $PROJ
+PROJID=`echo "$PROJ" | tr '\n' ' ' | sudo docker run -i stedolan/jq '.id'`
+echo Proj ID: $PROJID
+
 # SuT creation
 echo Creating SuT
-SUT=$(curl -s -H "Content-Type: application/json" -d @sutdesc.txt "$ELASTESTURL/api/sut")
+DESC=`sed "s/PROJID/$PROJID/" sutdesc.txt`
+SUT=$(curl -s -H "Content-Type: application/json" -d "$DESC" "$ELASTESTURL/api/sut")
 echo $SUT
 
 # T-Job creation
 echo Creating T-Job
-TJOB=$(curl -s -H "Content-Type: application/json" -d @tjobdesc.txt "$ELASTESTURL/api/tjob")
+DESC=`sed "s/PROJID/$PROJID/" tjobdesc.txt`
+TJOB=$(curl -s -H "Content-Type: application/json" -d "$DESC" "$ELASTESTURL/api/tjob")
 echo $TJOB
+
+TJOBID=`echo "$TJOB" | tr '\n' ' ' | sudo docker run -i stedolan/jq '.id'`
+echo TJob ID: $TJOBID
 
 # T-Job execution
 echo Executing T-Job
-TJOBEXEC=$(curl -s -H "Content-Type: application/json" -d '{"tJobParams": []}' "$ELASTESTURL/api/tjob/2/exec")
+TJOBEXEC=$(curl -s -H "Content-Type: application/json" -d '{"tJobParams": []}' "$ELASTESTURL/api/tjob/$TJOBID/exec")
 echo $TJOBEXEC
+TJOBEXECID=`echo "$TJOBEXEC" | tr '\n' ' ' | sudo docker run -i stedolan/jq '.monitoringIndex'`
+echo TJobEXEC ID: $TJOBEXECID
 
 # Getting result
 n=0
@@ -23,20 +44,20 @@ while [ $n -le 3000 ]
 do
 	n=$(( n+1 ))	 # increments $n
 	sleep 1
-	TJOBEXEC=$(curl -s "$ELASTESTURL/api/tjob/2/exec/1/result")
+	TJOBEXEC=$(curl -s "$ELASTESTURL/api/tjob/$TJOBID/exec/$TJOBEXECID/result")
     if [[ $TJOBEXEC = *"SUCCESS"* ]]; then
         echo Test successful
-        exit 0
+        cleanexit
     fi
     if [[ $TJOBEXEC = *"FAIL"* ]]; then
         echo Test failed
-        exit -1
+        cleanexit
     fi
     if [[ $TJOBEXEC = *"ERROR"* ]]; then
         echo Test erroneous
-        exit -1
+        cleanexit
     fi
 done
 
 echo Test took too long
-exit -1
+cleanexit
