4f625e47a9443635fd0a13a2fb735a4ae1aa6032
Felipe Gorostiaga <feligorostiaga@gmail.com>
2019-07-16 19:38:10 +0200

    Implementing undeployment of stampers and moms

--- go_EMS/eventproc/signals.go
+++ go_EMS/eventproc/signals.go
@@ -1,53 +1,57 @@
 package eventproc
 
 import (
-	dt "github.com/elastest/elastest-monitoring-service/go_EMS/datatypes"
-    pb "github.com/elastest/elastest-monitoring-service/protobuf"
-    "math/rand"
-    "strconv"
-    "fmt"
-	"github.com/elastest/elastest-monitoring-service/go_EMS/moms"
-    "github.com/elastest/elastest-monitoring-service/go_EMS/parsers/session"
-    "strings"
+  dt "github.com/elastest/elastest-monitoring-service/go_EMS/datatypes"
+  pb "github.com/elastest/elastest-monitoring-service/protobuf"
+  "math/rand"
+  "strconv"
+  "fmt"
+  "github.com/elastest/elastest-monitoring-service/go_EMS/moms"
+  "github.com/elastest/elastest-monitoring-service/go_EMS/parsers/session"
+  "strings"
 )
 
 var momengines map[int]dt.MoMEngine01 = make(map[int]dt.MoMEngine01)
 
 func DeploySignals01(signalsDef string) *pb.MomPostReply {
-    fmt.Println("Deploying signal defs: ", signalsDef)
-    reader := strings.NewReader(signalsDef)
-    monitorif, err := session.ParseReader("Monitoring Machine", reader)
-    if err != nil {
-        fmt.Println("deployment error: ", err.Error())
-        return &pb.MomPostReply{Deploymenterror:err.Error(), Momid:""}
-    }
-    momifs := monitorif.([]interface{})
-    moms := make([]session.MoM, len(momifs))
-    for i,momif := range momifs {
-        moms[i] = momif.(session.MoM)
-    }
-    momid := rand.Int()
-    deployRealMoM01(moms, momid)
-    fmt.Println("with momid: ", momid)
-    return &pb.MomPostReply{Deploymenterror:"", Momid:strconv.Itoa(momid)}
+  fmt.Println("Deploying signal defs: ", signalsDef)
+  reader := strings.NewReader(signalsDef)
+  monitorif, err := session.ParseReader("Monitoring Machine", reader)
+  if err != nil {
+    fmt.Println("deployment error: ", err.Error())
+    return &pb.MomPostReply{Deploymenterror:err.Error(), Momid:""}
+  }
+  momifs := monitorif.([]interface{})
+  moms := make([]session.MoM, len(momifs))
+  for i,momif := range momifs {
+    moms[i] = momif.(session.MoM)
+  }
+  momid := rand.Int()
+  deployRealMoM01(moms, momid)
+  fmt.Println("with momid: ", momid)
+  return &pb.MomPostReply{Deploymenterror:"", Momid:strconv.Itoa(momid)}
 }
 
 func deployRealMoM01(signaldefs []session.MoM, momid int) {
-    momengines[momid] = moms.StartEngine(signaldefs)
+  momengines[momid] = moms.StartEngine(signaldefs)
 }
 
-func UndeploySignals01(momid int) *pb.MomPostReply {
-    if engine,ok := momengines[momid]; ok {
-        delete(momengines, momid)
-        engine.Kill()
-        return &pb.MomPostReply{Deploymenterror:"", Momid:strconv.Itoa(momid)} // TODO change
-    }
-    return &pb.MomPostReply{Deploymenterror:"No such id", Momid:"momid"} // change
+func DeleteSignal(momidstr string) *pb.MomDeleteReply {
+  momid, err := strconv.Atoi(momidstr);
+  if err != nil {
+    return &pb.MomDeleteReply{Deletionerror:err.Error()}
+  }
+  if engine,ok := momengines[momid]; ok {
+    delete(momengines, momid)
+    engine.Kill()
+    return &pb.MomDeleteReply{Deletionerror:""}
+  }
+  return &pb.MomDeleteReply{Deletionerror:"No such id"}
 }
 
 func ProcessEvent(evt dt.Event) {
-    for _,engine := range momengines {
-        engine.Sampler.ProcessEvent(evt)
-    }
+  for _,engine := range momengines {
+    engine.Sampler.ProcessEvent(evt)
+  }
 }
 
--- swagger-go/implementation/implementation.go
+++ swagger-go/implementation/implementation.go
@@ -1,32 +1,32 @@
 package implementation
 
 import (
-	"net/http"
-	runtime "github.com/go-openapi/runtime"
-	middleware "github.com/go-openapi/runtime/middleware"
+  "net/http"
+  runtime "github.com/go-openapi/runtime"
+  middleware "github.com/go-openapi/runtime/middleware"
 
-	"../restapi/operations/subscribers"
-	"../models"
-	"fmt"
-	"math/rand"
-	"strconv"
-	"os"
-    "regexp"
-    "strings"
-    "os/exec"
+  "../restapi/operations/subscribers"
+  "../models"
+  "fmt"
+  "math/rand"
+  "strconv"
+  "os"
+  "regexp"
+  "strings"
+  "os/exec"
 )
 
 type LocalESEndpoint models.ESEndpoint
 type LocalRMQEndpoint models.RMQEndpoint
 
 type IEndpoint interface {
-	getInjectableString(subId string) string
+  getInjectableString(subId string) string
 }
 
 type SubscribeOk struct {
 
-    // In: body
-	subscriptionId string
+  // In: body
+  subscriptionId string
 }
 
 
@@ -35,38 +35,38 @@ type UnSubscribeOk struct {
 
 type SubscribeManyOk struct {
 
-    // In: body
-	subscriptionIds []string
+  // In: body
+  subscriptionIds []string
 }
 
 // WriteResponse to the client
 func (o SubscribeOk) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
 
-    rw.WriteHeader(200)
-	if err := producer.Produce(rw, o.subscriptionId); err != nil {
-		panic(err) // let the recovery middleware deal with this
-    }
+  rw.WriteHeader(200)
+  if err := producer.Produce(rw, o.subscriptionId); err != nil {
+    panic(err) // let the recovery middleware deal with this
+  }
 }
 
 func (o UnSubscribeOk) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
 
-    rw.WriteHeader(200)
-	if err := producer.Produce(rw, "ok"); err != nil {
-		panic(err) // let the recovery middleware deal with this
-    }
+  rw.WriteHeader(200)
+  if err := producer.Produce(rw, "ok"); err != nil {
+    panic(err) // let the recovery middleware deal with this
+  }
 }
 
 func (o SubscribeManyOk) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
 
-    rw.WriteHeader(200)
-	if err := producer.Produce(rw, o.subscriptionIds); err != nil {
-		panic(err) // let the recovery middleware deal with this
-    }
+  rw.WriteHeader(200)
+  if err := producer.Produce(rw, o.subscriptionIds); err != nil {
+    panic(err) // let the recovery middleware deal with this
+  }
 }
 
 func (endpoint LocalESEndpoint) getInjectableString(subId string) string {
-	template := `
-# SUBID %s
+  template := `
+  # SUBID %s
   if "%s" in [channels] {
     elasticsearch {
       hosts => ["%s:%v"]
@@ -74,14 +74,14 @@ func (endpoint LocalESEndpoint) getInjectableString(subId string) string {
       password => %s
     }
   }
-# ENDOF %s
+  # ENDOF %s
 }`
-  return fmt.Sprintf(template, subId, endpoint.Channel, endpoint.IP, endpoint.Port, endpoint.User, endpoint.Password, subId)
+return fmt.Sprintf(template, subId, endpoint.Channel, endpoint.IP, endpoint.Port, endpoint.User, endpoint.Password, subId)
 }
 
 func (endpoint LocalRMQEndpoint) getInjectableString(subId string) string {
-	template := `
-# SUBID %s
+  template := `
+  # SUBID %s
   rabbitmq {
     key => "%s"
     exchange => "%s"
@@ -93,100 +93,98 @@ func (endpoint LocalRMQEndpoint) getInjectableString(subId string) string {
     durable => true
     persistent => true
   }
-# ENDOF %s
+  # ENDOF %s
 }`
-  return fmt.Sprintf(template, subId, endpoint.Key, endpoint.Exchange, endpoint.ExchangeType, endpoint.User, endpoint.Password, endpoint.IP, endpoint.Port, subId)
+return fmt.Sprintf(template, subId, endpoint.Key, endpoint.Exchange, endpoint.ExchangeType, endpoint.User, endpoint.Password, endpoint.IP, endpoint.Port, subId)
 }
 
 func injectNewOutput(injStr string) {
-    // open input file
-	conffile := "/usr/share/logstash/pipeline/outlogstash.conf"
-    file, err := os.OpenFile(conffile, os.O_RDWR, 0755)
-    if err != nil {
-        panic(err)
+  // open input file
+  conffile := "/usr/share/logstash/pipeline/outlogstash.conf"
+  file, err := os.OpenFile(conffile, os.O_RDWR, 0755)
+  if err != nil {
+    panic(err)
+  }
+  // close file on exit and check for its returned error
+  defer func() {
+    if err := file.Close(); err != nil {
+      panic(err)
     }
-    // close file on exit and check for its returned error
-    defer func() {
-        if err := file.Close(); err != nil {
-            panic(err)
-        }
-    }()
+  }()
 
-	finfo, err := file.Stat()
-	if err != nil {
-		  panic(err)
-	}
+  finfo, err := file.Stat()
+  if err != nil {
+    panic(err)
+  }
 
-	fsize := finfo.Size()
-	//fmt.Printf("The file is %d bytes long", fsize)
+  fsize := finfo.Size()
+  //fmt.Printf("The file is %d bytes long", fsize)
 
-	if _, err := file.WriteAt([]byte(injStr), fsize-2); err != nil {
-		panic(err)
-	}
+  if _, err := file.WriteAt([]byte(injStr), fsize-2); err != nil {
+    panic(err)
+  }
 }
 
 func subscribeEndpoint(endpoint IEndpoint) SubscribeOk {
-	subId := strconv.Itoa(rand.Int())
-	injStr := endpoint.getInjectableString(subId)
-	injectNewOutput(injStr)
-	//fmt.Println(injStr)
-	return SubscribeOk{subId}
+  subId := strconv.Itoa(rand.Int())
+  injStr := endpoint.getInjectableString(subId)
+  injectNewOutput(injStr)
+  //fmt.Println(injStr)
+  return SubscribeOk{subId}
 }
 
 
 func SubscribeES(params subscribers.SubscribeElasticSearchParams) middleware.Responder {
-	endpoint := LocalESEndpoint(*params.Endpoint)
-	return subscribeEndpoint(endpoint)
+  endpoint := LocalESEndpoint(*params.Endpoint)
+  return subscribeEndpoint(endpoint)
 }
 
 func SubscribeRMQ(params subscribers.SubscribeRabbitMQParams) middleware.Responder {
-	endpoint := LocalRMQEndpoint(*params.Endpoint)
-	return subscribeEndpoint(endpoint)
+  endpoint := LocalRMQEndpoint(*params.Endpoint)
+  return subscribeEndpoint(endpoint)
 }
 
 func subscribeDefaultEndpoint(ep string, channel string) string {
-    edm_es := os.Getenv("ET_EDM_ELASTICSEARCH_API")
+  edm_es := os.Getenv("ET_EDM_ELASTICSEARCH_API")
 
-    // Sanity check
-    r, _ := regexp.Compile("http://[^:]+:[0-9]+/")
-    if !(r.MatchString(edm_es)) {
-        return "invalid_EDM_ES"
-    }
+  // Sanity check
+  r, _ := regexp.Compile("http://[^:]+:[0-9]+/")
+  if !(r.MatchString(edm_es)) {
+    return "invalid_EDM_ES"
+  }
 
 
-	edm_es = edm_es[7:len(edm_es)-1]
-	i := strings.Index(edm_es, ":")
-	ip := edm_es[:i]
-	port, _ := strconv.ParseInt(edm_es[i+1:], 10, 64)
+  edm_es = edm_es[7:len(edm_es)-1]
+  i := strings.Index(edm_es, ":")
+  ip := edm_es[:i]
+  port, _ := strconv.ParseInt(edm_es[i+1:], 10, 64)
 
-    switch ep {
-        case "persistence","dashboard":
-            return subscribeEndpoint(LocalESEndpoint(models.ESEndpoint{channel, ip, "changeme",  port, "elastic"})).subscriptionId
-        default:
-            return "unknown default endpoint"
-    }
+  switch ep {
+  case "persistence","dashboard":
+    return subscribeEndpoint(LocalESEndpoint(models.ESEndpoint{channel, ip, "changeme",  port, "elastic"})).subscriptionId
+  default:
+    return "unknown default endpoint"
+  }
 }
 
 func SubscribeElastestEndpoint(params subscribers.SubscribeElastestEndpointsParams) middleware.Responder {
-    channel := params.Endpoints.Channel
-    endpoints := params.Endpoints.Endpoints
+  channel := params.Endpoints.Channel
+  endpoints := params.Endpoints.Endpoints
 
-	subids := make([]string, len(endpoints))
-    for i, v := range endpoints {
-        subids[i] = subscribeDefaultEndpoint(v, channel)
-    }
-    return SubscribeManyOk{subids}
+  subids := make([]string, len(endpoints))
+  for i, v := range endpoints {
+    subids[i] = subscribeDefaultEndpoint(v, channel)
+  }
+  return SubscribeManyOk{subids}
 }
 
 func UnsubscribeHandler(params subscribers.UnsubscribeParams) middleware.Responder {
-    // sed -i '/SUBID 5577006791947779410/,/ENDOF 5577006791947779410/d' testfile.txt
-	cmd := "sed"
-	args := []string{"-i", "/SUBID "+params.SubID+"/,/ENDOF "+params.SubID+"/d", "/usr/share/logstash/pipeline/outlogstash.conf"}
-	if err := exec.Command(cmd, args...).Run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	return UnSubscribeOk{}
+  // sed -i '/SUBID 5577006791947779410/,/ENDOF 5577006791947779410/d' testfile.txt
+  cmd := "sed"
+  args := []string{"-i", "/SUBID "+params.SubID+"/,/ENDOF "+params.SubID+"/d", "/usr/share/logstash/pipeline/outlogstash.conf"}
+  if err := exec.Command(cmd, args...).Run(); err != nil {
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+  return UnSubscribeOk{}
 }
-
-
