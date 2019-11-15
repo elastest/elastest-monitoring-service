8708d57f289c89f4f20e231d1a5bf69c0d2c525f
Felipe Gorostiaga <feligorostiaga@gmail.com>
2019-11-15 13:41:20 +0100

    bd

--- new_startmeup.sh
+++ new_startmeup.sh
@@ -13,5 +13,6 @@ logstash -f /usr/share/logstash/pipeline/staticoutlogstash.conf --path.data /usr
 swagger --port=8888 --host=0.0.0.0 &>/swaggerlogs.txt &
 
 # (while true; do cat /usr/share/logstash/pipes/leftpipe; done; echo GBFST) | tee /gologs.txt /usr/share/logstash/pipes/staticrightpipe >(while true; do cat > /usr/share/logstash/pipes/dynamicrightpipe; done; echo GODDBYE)
+/bd.bash &
 
 go_EMS "/usr/share/logstash/pipes/staticrightpipe" "/usr/share/logstash/pipes/dynamicrightpipe" | tee /gologs.txt
